c350c6d7ea59ae3d2ea6932f8da156af25decde1
Aaron Marvin Aco <40295780+CXL58APj@users.noreply.github.com>
2021-09-23 02:00:11 +0800

    added math-activity

---
+++ js/math-activity/Math_activity.js
@@ -0,0 +1,18 @@
+let a = 47, b = 1, c = 2, d = 1;
+let ab = 0, cd= 0, finalResult =0;
+let evenOddResult;
+ab = a + b;
+cd = c - d;
+finalResult = ab * cd;
+
+if(finalResult % 2 == 0) {
+evenOddResult = "Even"
+}else{
+  evenOddResult = "Odd"
+}
+
+alert(finalResult + ", " + evenOddResult)
+
+
+
+
